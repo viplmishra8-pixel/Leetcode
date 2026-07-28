e960b3c46335764ef7bc9cb5be36abdf2b7924f8
Vipul Mishra <viplmishra8@gmail.com>
2026-07-28 12:35:45 +0530

    Time: 0 ms (100%), Space: 10.7 MB (24.29%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,10 +1,33 @@
 class Solution {
 public:
-    int helperFun(vector<int> nums , int i ){
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        if (n == 1) {
+            return nums[0];
+        }
+
+        vector<int> dp(n, 0);
+
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = max(dp[i - 1], nums[i] + dp[i - 2]);
+        }
+
+        return dp[n-1];        
+    }
+};
+
+/*
+     // GIVES MEMORY LIMIT EXCEEDED //
+     // also exponential timeComplexity //
+ int helperFun(vector<int> nums, int i ){
         if(i<0) { return 0 ;}
         return max(helperFun(nums , i-2)+ nums[i] ,helperFun( nums, i-1 ));
     }
     int rob(vector<int>& nums) {
         return helperFun(nums , nums.size()-1 );
     }
-};
+*/
