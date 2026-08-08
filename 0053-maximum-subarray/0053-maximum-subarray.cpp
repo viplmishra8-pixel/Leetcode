b1ff647744e6ee8f5f991d966b2c83bcc4ec2b46
Vipul Mishra <viplmishra8@gmail.com>
2026-08-08 14:29:37 +0530

    Time: 0 ms (100%), Space: 71.8 MB (53.62%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,29 +1,13 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int n = nums.size();
-        int res = nums[0];
-        int max_end = nums[0];
-        for(int i =  1 ; i < n  ;i++){
-            max_end = max( nums[i], max_end + nums[i]);
-            res = max(res , max_end);
+        int n = nums.size() ;
+        int sum = nums[0];
+        int maxi = nums[0];
+        for(int i = 1 ; i < n ; i++){
+            sum = max(nums[i] , sum+nums[i]);
+            maxi = max(sum , maxi);
         }
-        return res; 
+        return maxi ; 
     }
 };
-/*
-   int n = nums.size();
-        int max_sum = nums[0];
-        for(int i = 0 ; i < n ; i++){
-            int curr_sum = 0 ; 
-            for(int j = i ; j < n ;j ++){
-                curr_sum+=nums[j];
-                max_sum = max(curr_sum , max_sum);
-            }
-        }
-        return max_sum ;
-    }
-     // this will give TLE for large data set//  
-     // so better use kadens algorithm //
-     
-*/
